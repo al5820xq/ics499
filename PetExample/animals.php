d9c88cda0e30a8d3a4456dc2a93d7f6615efddf6
vinnyyo <60477624+vinnyyo@users.noreply.github.com>
2021-09-23 10:56:06 -0500

    working code

--- PetExample/animals.php
+++ PetExample/animals.php
@@ -1,8 +1,48 @@
 <?php
-    include "header.html";
+include "header.html";
     include "pet.php";
+?>
+<?php
+    require_once('mysqli_connect.php');
+    $animal = $_POST["animal"];
+    $name = $_POST["name"];
+    $color = $_POST["color"];
+    $insertQuery = "INSERT INTO testtable (petname, animaltype, color, identifier)
+     VALUES (?, ?, ?, NULL)";
+    $stmt = mysqli_prepare($dbc, $insertQuery);
+    mysqli_stmt_bind_param($stmt, "sss", $name, $animal, $color);
+    mysqli_stmt_execute($stmt);
+    $affected_rows = mysqli_stmt_affected_rows($stmt);
+    if($affected_rows == 1){
+        echo 'Student Entered';
+        mysqli_stmt_close($stmt);
+        //mysqli_close($dbc);
+    } else {
+        echo 'Error Occurred';
+        echo mysqli_error();
+        mysqli_stmt_close($stmt);
+        //mysqli_close($dbc);
+    }
 
-    /* require_once('mysqli_connect.php');
+    
+    if(1) {
+
+
+    } else {
+            echo "coul issue database query";
+            
+    }
+
+    $pet1 = new Pet($animal, $name, $color);
+    $pet2 = new Pet("Cat", "Garfield", "orange");
+    echo($pet1->toString());
+    echo($pet2->toString());
+?>
+
+<?php
+    
+
+     
 
     $query = "SElECT identifier, petname, animaltype, color FROM testtable";
 
@@ -20,8 +60,8 @@
         while($row = mysqli_fetch_array($response)){
             echo '<tr><td align="left">' . 
             $row['identifier'] . '</td><td align="left">' .  
-            $row['name'] . '</td><td align="left">' .  
-            $row['animal'] . '</td><td align="left">' .  
+            $row['petname'] . '</td><td align="left">' .  
+            $row['animaltype'] . '</td><td align="left">' .  
             $row['color'] . '</td><td align="left">';
 
             echo '</tr>';
@@ -32,19 +72,9 @@
         echo "coul issue database query";
         echo mysqli_error($dbc);
     }
-    mysqli_close($dbc); */
+    mysqli_close($dbc); 
 
     $user = "resource";
     $password = "petresource";
 
-?>
-
-<?php
-    $animal = $_POST["animal"];
-    $name = $_POST["name"];
-    $color = $_POST["color"];
-    $pet1 = new Pet($animal, $name, $color);
-    $pet2 = new Pet("Cat", "Garfield", "orange");
-    echo($pet1->toString());
-    echo($pet2->toString());
 ?>
